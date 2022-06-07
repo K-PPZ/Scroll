f82be103b4630cbf5ae9b5f9b20a72f6f99bf14c
KorenPapazian <kpapazian@edenschool.fr>
2022-06-07 17:05:01 +0200

    jsp

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let secService = document.getElementById("service");
+let buttonServ = document.getElementById("buttonServ");
+
+if (secService.style.top = "-10") {
+    buttonServ.style.backgroundColor = "red";
+} else {
+    
+}
